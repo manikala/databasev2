b468afdb47d69dee15bacf5f96e98c268c4ead08
Valeriy <manikala777@rambler.ru>
2022-03-05 14:07:15 +0300

    создание проекта (spring) и написание спецификациии

---
+++ src/main/java/store/store2/Store2Application.java
@@ -0,0 +1,13 @@
+package store.store2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Store2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Store2Application.class, args);
+    }
+
+}
---
+++ src/test/java/store/store2/Store2ApplicationTests.java
@@ -0,0 +1,13 @@
+package store.store2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Store2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
