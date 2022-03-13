36e008a1c0882494b570f0ea4cd962888debfe5a
Valeriy <manikala777@rambler.ru>
2022-03-13 22:52:33 +0300

    я в тупике

--- src/main/java/store/store2/service/UserServiceImpl.java
+++ src/main/java/store/store2/service/UserServiceImpl.java
@@ -1,11 +1,16 @@
 package store.store2.service;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import store.store2.dao.UserRepository;
 import store.store2.dto.UserDTO;
+
+import java.util.List;
+import java.util.Objects;
 
 @Service
 public class UserServiceImpl implements UserService{
@@ -20,11 +25,29 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public boolean save(UserDTO userDTO) {
-        if()
+        if(!Objects.equals(userDTO.getPassword(), userDTO.getMatchingPassword())) {
+            throw new RuntimeException("Password is not equals");
+        }
+        User user = User.builder()
+                .username(userDTO.getUsername())
+                .password(passwordEncoder.encode(userDTO.getPassword()))
+                .build();
+        userRepository.save(user);
+        return true;
+
     }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        User user = userRepository.findFirstByName(username);
+        if (user = null) {
+            throw new UsernameNotFoundException("User not found with name: " + username);
+
+        }
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword()
+        );
     }
 }
